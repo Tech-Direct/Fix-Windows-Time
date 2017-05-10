6f66a823c3f10c31f1e23117135ade60156dd0c4
James Watt <james@techdirect.io>
2017-05-10 13:04:28 -0400

    Replacing with new file

--- fixtime.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Run this script from your domain controller. (Exit to cancel.)
-pause
-w32tm /config /syncfromflags:yes
-manual /manualpeerlist:pool.ntp.org
-w32tm /config /reliable:32tm /config /update
-w32tm /resync
-net stop w32time
-net start w32time
