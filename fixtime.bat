1df25e50d06554ef77c832f20229b7ef4e2e51b0
James Watt <gtwy@users.noreply.github.com>
2017-05-10 12:54:53 -0400

    Update fixtime.bat

--- fixtime.bat
+++ fixtime.bat
@@ -2,7 +2,7 @@
 echo Run this script from your domain controller. (Exit to cancel.)
 pause
 w32tm /config /syncfromflags:manual /manualpeerlist:pool.ntp.org
-w32tm /config /reliable:yes
+w32tm /config /reliable:yes 
 w32tm /config /update
 w32tm /resync
 net stop w32time
