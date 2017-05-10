d23c7d99c1386d008b8d8411f851d1952ad00627
James Watt <gtwy@users.noreply.github.com>
2017-05-10 13:07:44 -0400

    Add files via upload

---
+++ fix_time.bat
@@ -0,0 +1,11 @@
+@echo off
+echo Run this script from your domain controller. (Exit to cancel.)
+pause
+w32tm /config /syncfromflags:manual /manualpeerlist:pool.ntp.org
+w32tm /config /reliable:yes
+w32tm /config /update
+w32tm /resync
+net stop w32time
+net start w32time
+echo NTP fixed. Please wait up to 15 minutes for all computers in domain to sync.
+pause
